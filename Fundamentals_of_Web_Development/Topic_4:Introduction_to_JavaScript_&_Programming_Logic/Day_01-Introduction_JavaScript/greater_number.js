8e662ea37cb84d8a295f30078445c1859bc253d2
Cesar Nogueira <cesardinogueira@gmail.com>
2022-02-02 18:18:02 -0300

    Topic_4-Day 01-Introduction_JS-Exercise Greater Number

---
+++ Fundamentals_of_Web_Development/Topic_4:Introduction_to_JavaScript_&_Programming_Logic/Day_01-Introduction_JavaScript/greater_number.js
@@ -0,0 +1,10 @@
+const num1 = 15;
+const num2 = 15;
+
+if (num1 > num2) {
+    console.log("O número", num1, "é maior que o número", num2);
+} else if (num2 > num1) {
+    console.log("O número", num2, "é maior que o número", num1);
+} else {
+    console.log("Os números são iguais");
+}
